4673ddbb9c943562dbbf1fada625ed9a217609b2
Mateus Lopes do Nascimento <mateusl1n1@gmail.com>
2020-01-23 13:53:42 -0300

    Setting up APP.js with Express and additional resources, initial view was setted up also.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
+
+const port = 3001;
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/', '/index.html/'));
+});
+
+app.listen(port, () => {
+    debug(`Server listening on ${chalk.green(port)}`);
+});
