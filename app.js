4e13a8c0ca1b428fe241d24dec1c60ef7fded183
Mateus Lopes do Nascimento <mateusl1n1@gmail.com>
2020-01-24 00:13:12 -0300

    Starting with Eslint

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const port = 3001;
 const app = express();
 
 app.use(morgan('tiny'));
-
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/jns', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/', '/index.html/'));
+  res.sendFile(path.join(__dirname, '/views/', '/index.html/'));
 });
 
 app.listen(port, () => {
-    debug(`Server listening on ${chalk.green(port)}`);
+  debug(`Server listening on ${chalk.green(port)}`);
 });
