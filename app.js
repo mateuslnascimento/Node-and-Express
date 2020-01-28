a7fe28a3345b916bfcd8e2f7f53f3a53b8813cb2
Mateus Lopes do Nascimento <mateusl1n1@gmail.com>
2020-01-27 23:18:57 -0300

    Starting with templates with pug & ejs

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const port = 3001;
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/jns', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/', '/index.html/'));
+  res.render('index', { title: 'Library', list: ['a', 'b'] });
 });
 
 app.listen(port, () => {
